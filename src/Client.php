2f055144d4e8b8eee9883622b2270d68bd160ecc
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2019-05-09 18:04:19 +0900

    add api base url and api key

--- src/Client.php
+++ src/Client.php
@@ -4,9 +4,14 @@ namespace Riotgames\Api;
 
 use GuzzleHttp\Client as GuzzleClient;
 
+/**
+ * @see https://developer.riotgames.com/
+ */
 class Client
 {
-    public function __construct()
+    private $baseUri = 'https://na1.api.riotgames.com';
+
+    public function __construct($apiKey)
     {
         $this->client = new GuzzleClient();
     }
