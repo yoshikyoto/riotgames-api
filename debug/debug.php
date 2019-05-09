d7c148c409dbe6f18330a6ed2cfe4b862ab1257a
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2019-05-09 19:14:26 +0900

    devug

--- debug/debug.php
+++ debug/debug.php
@@ -4,4 +4,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Yoshikyoto\Riotgames\Api\Client;
 
-$client = new Client('RGAPI-f21334f9-8a18-4f89-9e3f-847696be446d');
+$client = new Client('RGAPI-5769528a-c75b-472e-924e-bcfedbb670c3');
+$summoner = $client->getSummoner('うたかた');
+$masteries = $client->getChampionMastery($summoner->getId());
+var_dump($masteries);
